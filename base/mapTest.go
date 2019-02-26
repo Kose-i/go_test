ab38476469097b1379bbdd5e05406ba0572b08b7
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-26 10:20:29 +0900

    Add map

---
+++ base/mapTest.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+var numbers = map[string] int{
+  "one"   : 1,
+  "two"   : 2,
+  "three" : 3,
+}
+
+func main() {
+  hoge, ok := numbers["one"]
+  if ok == true {
+    fmt.Println("numbers[\"one\"]:",hoge)
+  } else {
+    fmt.Println("numbers['one'] is unexpected")
+  }
+}
