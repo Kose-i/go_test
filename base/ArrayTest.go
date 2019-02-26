1ea4ef711836c8f83d759d16157db560595cdcd7
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-26 09:50:06 +0900

    Modify ArrayTest slice

--- base/ArrayTest.go
+++ base/ArrayTest.go
@@ -2,11 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-  var test_array [100]int
-  fmt.Println(test_array[1])
-  for i := 0;i < 100;i+=1 {
-    fmt.Printf("%d ", i)
+func printArray(array [100]int) {
+  length := len(array)
+  fmt.Println("Array-size:", length)
+  for i := 0;i < length;i+=1 {
+    fmt.Printf("%d ", array[i])
   }
   fmt.Println("")
+}
+
+func set_three(array []int) {
+  for i := 0;i < 50;i+=1 {
+    array[i] = 3
+  }
+}
+
+func main() {
+  var test_array [100]int
+  test_array_cut := test_array[0:50]
+  fmt.Println("set_three:test_array[0:50]")
+  set_three(test_array_cut)
+  printArray(test_array)
 }
