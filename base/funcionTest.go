10a70d9f74a22f507795c8b04b7bcd0b86c1508d
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-26 10:43:08 +0900

    Add closure

--- base/funcionTest.go
+++ base/funcionTest.go
@@ -20,7 +20,7 @@ func test2(x int) (y int, z string, err bool) {
   return y, z,true
 }
 
-func main() {
+func function_12() {
   test1_int, test1_ok := test1(2)
   test2_int, test2_str, test2_err := test2(3)
   if test1_ok == true {
@@ -33,4 +33,12 @@ func main() {
   } else {
     fmt.Println("test2_err == true")
   }
+}
+func closure_test() {
+  var func_closure = func(str string)int{fmt.Println(str);return 3}
+  fmt.Println(func_closure("closure_test"))
+}
+func main() {
+  function_12()
+  closure_test()
 }
